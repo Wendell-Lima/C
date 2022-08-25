1a4be7d4c58ac10e22f985538a6c49f0333f3a8e
Wendell-Lima <wendelllima00@gmail.com>
2022-08-25 17:25:02 -0300

    Embaralhando as cartas
    Finalmente!!!

--- Trabalho 02/trabalho_Adivinhacao.c
+++ Trabalho 02/trabalho_Adivinhacao.c
@@ -7,42 +7,32 @@
 int main()
 {
     char cartas[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
-    char tab[M][M],aux[M][M],letra;
-    int i,j,igual = 0;
+    char tab[M][M],aux[M][M],letra[N];
+    int i,j,l,c,igual = 0;
 
     srand(time(NULL));
 
-    for(i=0;i<4;i++)
-        for(j=0;j<4;j++){
+    for(i=0;i<M;i++)
+        for(j=0;j<M;j++){
             aux[i][j] = '*';
             tab[i][j] = 'x';
         }
 
-    for(i=0;i<N;i++)
-    {
-        letra = cartas[rand() %8];  
+    for(i=0;i<N;i++){   
         for(j=0;j<2;j++){
-            
-            if(letra!='x'){
-                tab[][]
+            l = rand() % 4;
+            c = rand() % 4;
+            if(tab[l][c]=='x'){
+                tab[l][c] = cartas[i];
+            }
+            else{
+                j--;
             }
         }
+
     }
-        
+
     
-    printf("letra = %c",letra);
-    /*for(i=0;i<4;i++){
-        printf("\n");
-        for(j=0;j<4;j++){
-            printf("  ");
-            printf("%c",aux[i][j]);
-        }
-    }
-
-    for(i=0;i<N;i++)
-    printf("%c ",cartas[i]);
-    */   
-
     for(i=0;i<4;i++){
         printf("\n");
         for(j=0;j<4;j++){
