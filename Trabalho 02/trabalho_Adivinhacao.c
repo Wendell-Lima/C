ac4e17178177a5238c4e612908bc175336046b51
Wendell-Lima <wendelllima00@gmail.com>
2022-08-26 22:20:34 -0300

    2ª Etapa - OK!
    Segunda etapa do trabalho concluída, ainda possui alguns bugs, futuramente os corrigirei.

--- Trabalho 02/trabalho_Adivinhacao.c
+++ Trabalho 02/trabalho_Adivinhacao.c
@@ -7,17 +7,21 @@
 int main()
 {
     char cartas[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
-    char tab[M][M],aux[M][M],letra[N];
-    int i,j,l,c,igual = 0;
+    char tab[M][M],aux[M][M];
+    int i,j,l,c;
+    int linha,coluna,antiga_l=0,antiga_c=0,cont=0,jogadas=1,continua = 1;
 
     srand(time(NULL));
 
+    /* PREENCHENDO OS DOIS VETORES */
     for(i=0;i<M;i++)
         for(j=0;j<M;j++){
             aux[i][j] = '*';
             tab[i][j] = 'x';
         }
 
+
+    /* EMBARALHANDO CARTAS */
     for(i=0;i<N;i++){   
         for(j=0;j<2;j++){
             l = rand() % 4;
@@ -30,15 +34,61 @@ int main()
             }
         }
 
+    } 
+
+    /*Mensagem de boas vindas e instruções*/
+
+    printf("-----  Bem vindo ao jogo da memoria!  ----\n");
+    /*Colocarei depois um sistema que inicia ou finaliza o jogo conforme a intrução do usuário.*/
+
+    printf("Matriz:");
+    for(i=0;i<M;i++){
+        printf("\n");
+        for(j=0;j<M;j++){
+            printf("%c  ",tab[i][j]);
+        }
     }
 
-    
-    for(i=0;i<4;i++){
-        printf("\n");
-        for(j=0;j<4;j++){
-            printf("  ");
-            printf("%c",tab[i][j]);
+    while(continua == 1){
+        
+        printf("\nInforme a linha e a coluna: ");
+        scanf("%d %d",&linha,&coluna);
+
+        linha--;
+        coluna--;
+
+        aux[linha][coluna] = tab[linha][coluna];
+
+        printf("Matriz:");
+        for(i=0;i<M;i++){
+            printf("\n");
+            for(j=0;j<M;j++){
+                printf("%c  ",aux[i][j]);
+            }
         }
-    }   
+        if(aux[linha][coluna]=='*'){
+            if(jogadas%2==0){
+                if(aux[linha][coluna]!=aux[antiga_l][antiga_c]){
+                    aux[linha][coluna] = '*';
+                    aux[antiga_l][antiga_c] = '*';
+                    printf("\n---- Errooou! ----");
+                }
+                else{
+                    printf("\n---- Acertou! ----");
+                    cont++;
+                }
+            }
+        }
+        
+        
+        if(cont==8){
+            printf("\n\nParabens!!! \nVoce venceu :D");
+            continua = 0;
+        }
+        antiga_l = linha;
+        antiga_c = coluna;
+        jogadas++;
+    }
+
     return 0;
 }
