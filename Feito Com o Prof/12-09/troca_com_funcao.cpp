7aa71a51bc876d6bc8d3391f109b39adf6f1307e
Wendell-Lima <83967725+Wendell-Lima@users.noreply.github.com>
2022-09-12 20:25:14 -0300

    Add files via upload

--- Feito Com o Prof/12-09/troca_com_funcao.cpp
+++ Feito Com o Prof/12-09/troca_com_funcao.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 	
+// Procedimento void!!! sem return
+
 void troca(int *X, int *Y){
 	int aux;
 	
@@ -9,13 +11,32 @@ void troca(int *X, int *Y){
 	*Y = aux;
 }
 
+// Função para cálculo do fatorial
+
+int Fat(int N){
+	int R, i;
+	
+	if (N<0) return -1; //situação de erro, o cara passou valor negativo
+	
+	R=1;
+	i=2;
+	while(i<=N){
+		R=R*i;
+		i++;
+	}
+		
+	return R;
+}
+
 int main(){
 	int A, B;
 	printf("Informe os valores para A e B = ");
 	scanf("%d %d", &A, &B);
 	
-	troca(&A,&B);
+	troca(&A,&B); /*X aponta para o A, Y aponta para o B*/
 	printf("A:%d B:%d\n", A, B);
 	
+	
+	printf("Fatorial de %d = %d\n", A, Fat(A));
 	return 0;
 }
