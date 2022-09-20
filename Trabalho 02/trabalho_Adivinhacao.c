f6d906b9dbce164d6c46408b5657ad237e9a13de
Wendell-Lima <wendelllima00@gmail.com>
2022-09-20 17:57:25 -0300

    Exercicios Procedimentos e Funções em C

--- Trabalho 02/trabalho_Adivinhacao.c
+++ Trabalho 02/trabalho_Adivinhacao.c
@@ -4,12 +4,17 @@
 #define N 8
 #define M 4
 
+/*
+    Nome: Wendell Maciel de Lima R.A. = 22021423
+*/
+
+
 int main()
 {
     char cartas[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
     char tab[M][M],aux[M][M];
     int i,j,l,c;
-    int linha,coluna,antiga_l=0,antiga_c=0,cont=0,jogadas=1,continua = 1;
+    int linha,coluna,antiga_l=0,antiga_c=0,cont=0,jogadas=1,continua = 0;
 
     srand(time(NULL));
 
@@ -39,15 +44,24 @@ int main()
     /*Mensagem de boas vindas e instruções*/
 
     printf("-----  Bem vindo ao jogo da memoria!  ----\n");
+    printf("\nPara jogar digite apenas numeros entre 1 e 4, tanto para linha como coluna.");
+    printf("\nDigite 1 para iniciar: ");
+    scanf("%d",&continua);
     /*Colocarei depois um sistema que inicia ou finaliza o jogo conforme a intrução do usuário.*/
 
     printf("Matriz:");
-    for(i=0;i<M;i++){
-        printf("\n");
-        for(j=0;j<M;j++){
-            printf("%c  ",tab[i][j]);
+    if(continua==1){
+        for(i=0;i<M;i++){
+            printf("\n");
+            for(j=0;j<M;j++){
+                printf("%c  ",aux[i][j]);
+                /*
+                    Caso queira ver a matriz antes para testar, troque o aux[i][j] por tab[i][j].
+                */
+            }
         }
     }
+    
 
     while(continua == 1){
         
@@ -66,20 +80,18 @@ int main()
                 printf("%c  ",aux[i][j]);
             }
         }
-        if(aux[linha][coluna]=='*'){
-            if(jogadas%2==0){
-                if(aux[linha][coluna]!=aux[antiga_l][antiga_c]){
-                    aux[linha][coluna] = '*';
-                    aux[antiga_l][antiga_c] = '*';
-                    printf("\n---- Errooou! ----");
-                }
-                else{
-                    printf("\n---- Acertou! ----");
-                    cont++;
-                }
+        
+        if(jogadas%2==0){
+            if(aux[linha][coluna]!=aux[antiga_l][antiga_c]){
+                aux[linha][coluna] = '*';
+                aux[antiga_l][antiga_c] = '*';
+                printf("\n---- Errooou! ----");
+            }
+            else{
+                printf("\n---- Acertou! ----");
+                cont++;
             }
         }
-        
         
         if(cont==8){
             printf("\n\nParabens!!! \nVoce venceu :D");
